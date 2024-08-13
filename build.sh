afe6288661c503c9c704aecc871de81e9a845918
lena <lena@lena-lenovo.(none)>
2024-08-14 02:12:12 +0300

    updatesssssss

--- build.sh
+++ build.sh
@@ -4,3 +4,16 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+pip install --upgrade frozendict experta
+pip freeze > requirements.txt
+# Remove existing virtual environment
+rm -rf .venv
+
+# Create a new virtual environment
+python -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate  # or .venv\Scripts\activate for Windows
+
+# Install dependencies
+pip install -r requirements.txt
