7539f28b95891f6f03c1eff38c48caedb42d8621
lena <lena@lena-lenovo.(none)>
2024-08-14 11:31:19 +0300

    last kkkk

--- build.sh
+++ build.sh
@@ -7,13 +7,14 @@ pip install -r requirements.txt
 pip install experta
 pip freeze > requirements.txt
 # Remove existing virtual environment
-rm -rf .venv
+# rm -rf .venv
+sed -i 's/from collections import Mapping/from collections.abc import Mapping/' $(pip show frozendict | grep Location | cut -d ' ' -f 2)/frozendict/__init__.py
 
 # Create a new virtual environment
-python -m venv .venv
+# python -m venv .venv
 
 # Activate the virtual environment
-source .venv/bin/activate  # or .venv\Scripts\activate for Windows
+# source .venv/bin/activate  # or .venv\Scripts\activate for Windows
 
 # Install dependencies
 pip install fastapi uvicorn
